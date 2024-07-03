3b71243ae6679516e97d0946d6625cc2f667ea3c
Moaz El Gandy <moazhazem2005@icloud.com>
2024-07-03 17:35:39 +0300

    chore: Update vercel.json to specify a new destination for rewrites

--- index.js
+++ index.js
@@ -17,3 +17,5 @@ app.use("/comments", commentRouter);
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+module.exports = app;
