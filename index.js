86b87763ee930932181b33fbabeb805b0a2d074d
Moaz El Gandy <moazhazem2005@icloud.com>
2024-07-03 17:44:04 +0300

    chore: Remove unused export of app in index.js

--- index.js
+++ index.js
@@ -17,5 +17,3 @@ app.use("/comments", commentRouter);
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-module.exports = app;
