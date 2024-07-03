82e17a21f504f3375618b6a2e7f2a5f66838b172
Moaz El Gandy <moazhazem2005@icloud.com>
2024-07-03 18:09:54 +0300

    chore: Update port number in index.js to 8080

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import postRouter from "./src/modules/posts/post.routes.js";
 import commentRouter from "./src/modules/comments/comment.routes.js";
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 db();
 
